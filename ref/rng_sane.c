ffca7b6f84268b5ee73000bde3a275a3874b5bec
dis <dis@firemail.cc>
2018-03-07 13:05:55 +0100

    Fix sane rng state restarts.

--- ref/rng_sane.c
+++ ref/rng_sane.c
@@ -1,7 +1,9 @@
-// avoid the utter aes nonsense in rng.c, and just do buffered output from shake.
-// that makes verification elsewhere much less painful
+// avoid the utter aes nonsense in rng.c (it does that because NIST requieremnt)
+// and just do buffered output from shake. that makes implementation correctness
+// verification elsewhere a bit less painful
 
 #include "fips202.h"
+#include "params.h"
 static uint64_t state[25];
 static unsigned char pending[SHAKE256_RATE];
 static int ptr = SHAKE256_RATE;
@@ -11,12 +13,15 @@ void randombytes_init(unsigned char *entropy_input,
                  int security_strength)
 {
     shake256_absorb(state, entropy_input, 48);        
+    ptr = SHAKE256_RATE;
 }
 
 int
 randombytes(unsigned char *x, unsigned long long xlen)
 {
     unsigned char rem[SHAKE256_RATE];
+    int orig = xlen;
+    unsigned char *ox = x;
     do {
 	for (;(ptr < SHAKE256_RATE) && xlen; xlen--)
 	    *x++ = pending[ptr++];
